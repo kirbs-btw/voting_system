308a6e2e4100fb17dfd446eee2f94951980894c6
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-10-05 19:27:26 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Candidate candidateList[] = {};
+
+        // hardcoded candidates
+        Candidate PersonA = new Candidate(0.1f, -0.7f, 0);
+        Candidate PersonB = new Candidate(-0.8f, 0.1f, 1);
+        Candidate PersonC = new Candidate(0.2f, -0.1f, 2);
+
+        Candidate candidateListNew[] = new Candidate[candidateList.length + 1]
+
+
+    }
+}
