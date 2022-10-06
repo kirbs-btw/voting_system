b3c18372fd8d8ae6572bdac445c67e5793963938
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-10-06 10:13:19 +0200

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,15 @@
+
+public class Person{
+    public float opinionA;
+    public float opinionB;
+    public int candidateNum;
+
+    public void checkValues(){
+        /*
+        System.out.println("Num: "+ candidateNum);
+        System.out.println("opinionA: "+ opinionA);
+        System.out.println("opinionB: "+ opinionB);
+         */
+        System.out.println(candidateNum + " " + opinionA + " " + opinionB);
+    }
+}
