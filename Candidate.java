b8a612c419d4d020e131d1c5f0bf8ae68c463daa
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-10-07 17:15:44 +0200

    Add files via upload

--- Candidate.java
+++ Candidate.java
@@ -1,10 +1,10 @@
 class Candidate extends Person{
-    public Candidate(float a, float b, int num){
+    public Candidate(float a, float b, int id){
         //Random random = new Random();
         // opinionA = random.nextFloat(-1, 1);
         //opinionB = random.nextFloat(-1, 1);
         opinionA = a;
         opinionB = b;
-        candidateNum = num;
+        num = id;
     }
 }
