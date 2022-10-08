4de88c35ed756f69e1ea64e06401f2d0a3364811
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-10-08 15:32:42 +0200

    Add files via upload

--- Elector.java
+++ Elector.java
@@ -1,10 +1,11 @@
 import java.util.Random;
 
 class Elector extends Person{
-    public Elector(int id){
+    public Elector(int id, int numOpinions){
         Random random = new Random();
-        opinionA = random.nextFloat(-1, 1);
-        opinionB = random.nextFloat(-1, 1);
+        for (int i = 0; i < numOpinions; i++){
+               opinions.add(random.nextFloat());
+        }
         num = id;
     }
 }
