5249c53cee4fd679538d4e2edaecd77fbef7e0fc
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-10-09 16:14:56 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -8,16 +9,37 @@ public class Main {
         }
     }
 
-    public static void compare(List<Person> electors, List<Person>candidates){
+    public static List<List<Float>> distance(List<Person> electors, List<Person>candidates){
+        /*
+         * comparing the difference of the candidates and electors
+         */
+
+        List<List<Float>> distSetList = new ArrayList<>();
+
         for (Person elector : electors){
+            List<Float> distSet = new ArrayList<>();
             for (Person candidate : candidates){
+                /*
+                *  1. Calculation the difference
+                *  2. Calculating the distance ot of the values of the vector
+                */
+                float diffSqareSum = 0f;
+
                 for (int i = 0; i < elector.opinions.size(); i++){
                     float opinionDiff = elector.opinions.get(i) - candidate.opinions.get(i);
-                    System.out.println(elector.num + " " + candidate.num + " Opinions: " + i + " : " + i);
-                    System.out.println(opinionDiff);
+                    diffSqareSum = diffSqareSum + opinionDiff * opinionDiff;
                 }
+
+                float dist = (float) Math.sqrt(diffSqareSum);
+                distSet.add(dist);
             }
+            distSetList.add(distSet);
         }
+        return distSetList;
+    }
+
+    public static float max(List<Float> arr){
+        return arr.stream().sorted(Comparator.reverseOrder()).toList().get(0);
     }
 
 
@@ -25,6 +47,7 @@ public class Main {
         List<Person> candidateList = new ArrayList<>();
 
         // hardcoded candidates
+        // number of opinions of the candidate and the electors have to be the same!
         Candidate PersonA = new Candidate(0, 0.1f, -0.7f);
         Candidate PersonB = new Candidate(1, -0.8f, 0.1f);
         Candidate PersonC = new Candidate(2, 0.2f, -0.1f);
@@ -39,10 +62,7 @@ public class Main {
             electorList.add(person);
         }
 
-        //checkList(candidateList);
-        //checkList(electorList);
-
-        compare(electorList, candidateList);
-
+        List<List<Float>> distSetList = distance(electorList, candidateList);
+        // set of the distances between elector and ever candidate
     }
 }
